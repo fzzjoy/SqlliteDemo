cae7680f90a84059cfc44679a61e7844fd483a8c
fzzjoy <769193110@qq.com>
2019-09-28 11:42:07 +0800

    完善数据库管理,支持查找/编辑/删除

--- search_widget.cpp
+++ search_widget.cpp
@@ -1,6 +1,9 @@
 #include "search_widget.h"
 #include "ui_search_widget.h"
 #include "utils.h"
+#include <QSqlQuery>
+#include <QMessageBox>
+#include <QSqlError>
 
 using namespace std;
 
@@ -30,4 +33,56 @@ void SearchWidget::showEvent(QShowEvent *event)
 
     ui->userTableView->setModel(model_.get());
     ui->userTableView->show();
+}
+
+void SearchWidget::execAction(const QString &action)
+{
+    if(!model_->submitAll())
+    {
+        QString err_info = tr("%1失败[%2]").arg(action).arg(model_->lastError().text());
+        QMessageBox::critical(this, tr("错误提示"), err_info, QMessageBox::Ok | QMessageBox::Default, 0);
+        return;
+    }
+    else
+    {
+        QMessageBox::information(this, tr("提示"), tr("%1成功").arg(action), QMessageBox::Ok | QMessageBox::Default, 0);
+    }
+}
+
+void SearchWidget::on_searchPushButton_clicked()
+{
+    QString sql;
+    if(ui->searchLineEdit->text().isEmpty())
+    {
+        sql = "";
+    }
+    else
+    {
+        sql = QString("name='%1'").arg(ui->searchLineEdit->text());
+    }
+
+    model_->setFilter(sql);
+    model_->select();
+}
+
+void SearchWidget::on_savePushButton_clicked()
+{
+    execAction(std::move(tr("保存")));
+}
+
+void SearchWidget::on_deletePushButton_clicked()
+{
+    QMessageBox::StandardButton reply = QMessageBox::question(this, tr("提示"), tr("确定删除选中记录?"), QMessageBox::Yes | QMessageBox::No);
+    if (reply == QMessageBox::No)
+    {
+        return;
+    }
+
+    QItemSelectionModel *selections = ui->userTableView->selectionModel();
+    QModelIndexList selected = selections->selectedIndexes();
+    foreach (QModelIndex index, selected)
+    {
+        model_->removeRow(index.row());
+    }
+    execAction(std::move(tr("删除")));
 }
--- search_widget.h
+++ search_widget.h
@@ -18,8 +18,16 @@ public:
     explicit SearchWidget(const QSqlDatabase &base, QWidget *parent = nullptr);
     ~SearchWidget();
 
+private slots:
+    void on_searchPushButton_clicked();
+
+    void on_savePushButton_clicked();
+
+    void on_deletePushButton_clicked();
+
 private:
     void showEvent(QShowEvent *event);
+    void execAction(const QString &action);
 
     Ui::SearchWidget                    *ui;
     std::unique_ptr<QSqlTableModel>     model_;
